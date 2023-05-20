228c3a0f87a8fa05bc8b212a71ecec4587049ca7
Tamir Zchut <tamirzc@gmail.com>
2023-05-20 23:47:06 +0300

    update gui

--- gui.py
+++ gui.py
@@ -6,6 +6,10 @@ def windowCreate(titleWin):
     layout=[[sg.Text("Weather Information", font=('Times New Roman', 25))]
             ,[sg.Text("City:"),
               sg.In(size=(25, 1), enable_events=True,key="-CITY-")]
+            ,[sg.Text("Language:"),sg.Radio("English","lan",default=True),sg.Radio("Spanish","lan",default=False),sg.Radio("French","lan",default=False),sg.Radio("Italian","lan",default=False)]
+            ,[sg.Text("Units:"),sg.Radio("Celsius","units",default=True),sg.Radio("Fahrenheit","units",default=False),sg.Radio("Kelvin","units",default=False)]
+            ,[sg.Text("API key:"),
+              sg.In(size=(25, 1), enable_events=True,key="-API-")]
             ,[sg.Button("Check the weather!")]
             ,[sg.Button("Exit")]]
     
